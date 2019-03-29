917fc2caf7c655a73d3c4510ddb93fe16d07b895
UtrillaJC <utri1990@gmail.com>
2019-03-29 07:36:39 +0100

    REST Framework app disabled

--- Server/settings.py
+++ Server/settings.py
@@ -54,6 +54,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKEND': (
