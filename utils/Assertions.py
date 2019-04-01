7da2ed06330e77a050c1bd4e0a2934c87e5c232e
Iriabow <guilderwars@gmail.com>
2019-04-01 19:06:48 +0200

    Temporal

--- utils/Assertions.py
+++ utils/Assertions.py
@@ -1,10 +1,28 @@
 from rest_framework import serializers
+from rest_framework.exceptions import APIException
 
 
-#def _assert_true(assertion: bool, http_error: int,)
+class Assertions:
 
 
-#Old functions
+
+    def assert_true_raise400():
+    def assert_true_raise401():
+    def assert_true_raise403():
+    def assert_true_raise404():
+
+
+    @staticmethod
+    def __assert_true(assertion: bool, http_error: int, code: str, details: str):
+        if not assertion:
+            print(code)
+            print(details)
+            exception = APIException(code=code, details=details)
+            exception.status_code = http_error
+            raise exception
+
+
+#Old functions - deprecated
 def assert_true(boolean_value, message):
     if not boolean_value:
         print(message)
