0efe46eccaeac02c59ca8465d6c21f2c777bed7e
Iriabow <guilderwars@gmail.com>
2019-04-02 14:00:45 +0200

    Full Assertions class

--- utils/Assertions.py
+++ utils/Assertions.py
@@ -4,16 +4,24 @@ from rest_framework.exceptions import APIException
 
 class Assertions:
 
-
-
-    def assert_true_raise400():
-    def assert_true_raise401():
-    def assert_true_raise403():
-    def assert_true_raise404():
-
+    @staticmethod
+    def assert_true_raise400(assertion: bool, details: dict):
+        Assertions.__assert_true(assertion, 400, {"code": "invalid data"}, details)
 
     @staticmethod
-    def __assert_true(assertion: bool, http_error: int, code: str, details: str):
+    def assert_true_raise401(assertion: bool, details: dict = {"message": "No authenticate"} ):
+        Assertions.__assert_true(assertion, 401, {"code": "No authenticate"}, details)
+
+    @staticmethod
+    def assert_true_raise403(assertion: bool, details: dict = {"code": "Denied persmission"}):
+        Assertions.__assert_true(assertion, 403, {"code": "Denied persmission"}, details)
+
+    @staticmethod
+    def assert_true_raise404(assertion: bool):
+        Assertions.__assert_true(assertion, 404, {"code": "Not Found"}, {"code": "Not Found"})
+
+    @staticmethod
+    def __assert_true(assertion: bool, http_error: int, code: dict , details: dict):
         if not assertion:
             print(code)
             print(details)
