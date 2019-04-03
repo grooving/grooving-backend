446c3c76a23d70875b44d1be4dfb28f8506b0fbe
davidromesp <davidromesp@hotmail.com>
2019-04-03 15:02:01 +0200

    Arreglo populate y otros

--- offer/views.py
+++ offer/views.py
@@ -3,7 +3,7 @@ from django.core.exceptions import PermissionDenied
 from utils.authentication_utils import get_logged_user, get_user_type, get_customer, get_artist
 from rest_framework.response import Response
 from rest_framework import generics
-from .serializers import OfferSerializer, CodeSerializer
+from .serializers import OfferSerializer
 from rest_framework import status
 from django.http import Http404
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
