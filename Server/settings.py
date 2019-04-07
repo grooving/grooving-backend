a9635992b82c7b7d6dd1ba19e0956cfe09973528
Juan Carlos Utrilla <utri1990@gmail.com>
2019-04-07 21:22:08 +0200

    Notifications system added

--- Server/settings.py
+++ Server/settings.py
@@ -86,7 +86,8 @@ ROOT_URLCONF = 'Server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'utils/notifications/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -153,9 +154,18 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Email configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'grupogrooving@gmail.com'
+EMAIL_HOST_PASSWORD = '94TDtF4zG2t4Cxy'
+
 django_heroku.settings(locals())
