256a274d81ae6a3e99c145f381033eafd1fbc88c
Iriabow <guilderwars@gmail.com>
2019-04-11 17:54:40 +0200

    Update populate.py

--- populate.py
+++ populate.py
@@ -91,6 +91,11 @@ def save_data():
                     "Conditions.</p>",
 
                     logo='',
+                    aboutUs="<h1>About us</h1>" +
+							"<p> At Grooving, we all come to work every day because we want to solve the biggest " +
+                            "problems for the artists: empower artists by giving visibility and improving their " +
+                            "monetization. What is our mission? Simplifying the search and hiring made easy.</p>" +
+							"<p>With all this, we can ensure that <b>Grooving makes the difference</b>.</p> ",
 
                     termsText="<h1>Terms & conditions</h1>" +
                               "<p>The conditions of use of the web page, the rules of use and the use of " +
