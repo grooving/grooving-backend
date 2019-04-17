060aae3b9c85f6349b7db7f2f1c0665671dae48d
Juan Rodríguez Dueñas <juaniito.rodriiguez92@gmail.com>
2019-04-17 23:42:59 +0200

    Split login

--- login/views.py
+++ login/views.py
@@ -32,10 +32,10 @@ class LoginManager(ObtainAuthToken):
                     customer = Customer.objects.get(user_id=token.user.id)
                     customer.user.last_login = last_login
                     serialized = LoginSerializer(customer)
-                elif Admin.objects.filter(user_id=token.user.id).first():
-                    admin = Admin.objects.get(user_id=token.user.id)
-                    admin.user.last_login = last_login
-                    serialized = LoginSerializer(admin)
+                # elif Admin.objects.filter(user_id=token.user.id).first():
+                #     admin = Admin.objects.get(user_id=token.user.id)
+                #     admin.user.last_login = last_login
+                #     serialized = LoginSerializer(admin)
                 else:
                     return Response({"User not found"}, status=status.HTTP_400_BAD_REQUEST)
                 return Response(serialized.data, status=status.HTTP_200_OK, headers=headers)
@@ -43,6 +43,26 @@ class LoginManager(ObtainAuthToken):
                 return Response({"Token not get/created"}, status=status.HTTP_400_BAD_REQUEST)
 
 
+class AdminLoginManager(ObtainAuthToken):
+    # permission_classes = (IsAuthenticated,)
+    queryset = User.objects.all()
+    # serializer_class = UserSerializer
 
-
-
+    def post(self, request):
+        authTokenSerializer = AuthTokenSerializer(data=request.data)
+        if authTokenSerializer.is_valid(raise_exception=True):
+            user = authTokenSerializer.validated_data['user']
+            last_login = user.last_login
+            update_last_login(None, user)
+            token, created = Token.objects.get_or_create(user=user)
+            if token.key is not None:
+                headers = {'x-auth': token.key}
+                if Admin.objects.filter(user_id=token.user.id).first():
+                    admin = Admin.objects.get(user_id=token.user.id)
+                    admin.user.last_login = last_login
+                    serialized = LoginSerializer(admin)
+                else:
+                    return Response({"Admin not found"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(serialized.data, status=status.HTTP_200_OK, headers=headers)
+            else:
+                return Response({"Token not get/created"}, status=status.HTTP_400_BAD_REQUEST)
