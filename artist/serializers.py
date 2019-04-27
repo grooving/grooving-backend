6542da16a74a3b107d9ea3c1977491aa5ebacdf2
UtrillaJC <utri1990@gmail.com>
2019-04-27 06:35:35 +0200

    Artist Internationalizated & fixed 500 Errors

--- artist/serializers.py
+++ artist/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from Grooving.models import Artist, Portfolio,Calendar
+from Grooving.models import Artist, Portfolio, Calendar
 from user.serializers import UserSerializer
 from portfolio.serializers import ArtisticGenderSerializer
 from django.contrib.auth.hashers import make_password
@@ -8,18 +8,17 @@ from user.serializers import UserRegisterSerializer
 from utils.Assertions import Assertions
 from utils.strings import Strings
 from utils.notifications.notifications import Notifications
+from artist.internationalization import translate
+from utils.utils import check_accept_language, check_special_characters_and_numbers
 
 
 class EvenShorterPortfolioSerializer(serializers.ModelSerializer):
-
     class Meta:
-
         model = Portfolio
         fields = ('artisticName',)
 
 
 class ArtistInfoSerializer(serializers.HyperlinkedModelSerializer):
-
     user = UserSerializer(read_only=True)
     artisticName = EvenShorterPortfolioSerializer(read_only=True, source='portfolio')
 
@@ -31,15 +30,14 @@ class ArtistInfoSerializer(serializers.HyperlinkedModelSerializer):
 
 class ShortPortfolioSerializer(serializers.ModelSerializer):
     artisticGender = ArtisticGenderSerializer(read_only=True, many=True)
-    class Meta:
 
+    class Meta:
         model = Portfolio
         fields = ('id', 'artisticName', 'artisticGender')
         search_fields = ['artisticName', 'artisticGender__name']
 
 
 class ListArtistSerializer(serializers.HyperlinkedModelSerializer):
-
     portfolio = ShortPortfolioSerializer(read_only=True)
 
     class Meta:
@@ -55,66 +53,18 @@ class ArtistSerializer(serializers.ModelSerializer):
         depth = 2
         model = Artist
         user = UserRegisterSerializer()
-        fields = ('user', 'artisticName', 'phone', 'photo','paypalAccount')
+        fields = ('user', 'artisticName', 'phone', 'photo', 'paypalAccount')
 
-    def save(self):
+    def save(self, request):
 
         artist = self._service_create_artist(self.initial_data)
         Notifications.send_email_welcome(artist.user.id)
         return artist
 
-    def update(self, pk):
+    def update(self, request, pk):
 
-        artist = self._service_update_artist(self.initial_data, pk)
+        artist = self._service_update_artist(self.initial_data, request, pk)
         return artist
-
-    @staticmethod
-    def _service_update_artist(json: dict, pk):
-        Assertions.assert_true_raise400(json, {'error': "Empty form is not valid"})
-        artist = Artist.objects.get(pk=pk)
-        artist.phone = json.get('phone')
-        user = artist.user
-        user.first_name = json.get('first_name').strip()
-        user.last_name = json.get('last_name').strip()
-        artist.paypalAccount = json.get('paypalAccount')
-        if artist.paypalAccount:
-            Assertions.assert_true_raise400('@' in artist.paypalAccount and '.' in artist.paypalAccount,
-                                        {'error': "Invalid paypalAccount"})
-        photo = json.get('photo')
-        Assertions.assert_true_raise400(user.first_name, {'error': "First name not provided"})
-        Assertions.assert_true_raise400(user.last_name, {'error': "Last name not provided"})
-        if artist.phone:
-            Assertions.assert_true_raise400(artist.phone.isnumeric(), {'error': "Phone must be a number"})
-            Assertions.assert_true_raise400(len(artist.phone) == 9, {'error': "Phone length must be 9 digits"})
-
-        Assertions.assert_true_raise400(len(user.first_name) > 1,
-                                        {'error': "First name is too short"})
-        Assertions.assert_true_raise400(len(user.last_name) > 1,
-                                        {'error': "Last name is too short"})
-        if photo:
-            Assertions.assert_true_raise400(photo.startswith('http'), {'error': 'Invalid photo url,'
-                                                                                ' the photo must start with http'})
-            Assertions.assert_true_raise400(Strings.url_is_an_image(photo), {'error': 'Invalid photo url,'
-                                                              ' the photo must end with an image extension'})
-
-        artisticName = json.get('artisticName')
-
-        Assertions.assert_true_raise400(artisticName, {'error': 'ERROR_NO_ARTISTIC_NAME'})
-        Assertions.assert_true_raise400(artisticName != "", {'error': 'ERROR_NO_ARTISTIC_NAME'})
-
-        try:
-
-            portfolio = Portfolio.objects.get(artist=artist)
-
-            portfolio.artisticName = artisticName
-
-            user.save()
-            artist.user = user
-            portfolio.save()
-            return artist
-        except Portfolio.DoesNotExist:
-
-            Assertions.assert_true_raise400(False, {'error': 'ERROR_NOTFOUND_PORTFOLIO'})
 
     @staticmethod
     def _service_create_artist(json: dict):
@@ -132,68 +82,135 @@ class ArtistSerializer(serializers.ModelSerializer):
         return artist
 
     @staticmethod
-    def validate_artist(request):
+    def _service_update_artist(json: dict, request, pk):
+        language = check_accept_language(request)
 
-        user_names = User.objects.values_list('username', flat=True)
-        emails = User.objects.values_list('email', flat=True)
-        password = request.data.get("password").strip()
+        Assertions.assert_true_raise400(json, translate(language, "ERROR_EMPTY_JSON"))
+
+        artist = Artist.objects.get(pk=pk)
+        artist.phone = json.get('phone')
+        user = artist.user
+
+        Assertions.assert_true_raise400(json.get('first_name'), translate(language, "ERROR_EMPTY_FIRST_NAME"))
+        Assertions.assert_true_raise400(json.get('last_name'), translate(language, "ERROR_EMPTY_LAST_NAME"))
+
+        user.first_name = json.get('first_name').strip()
+        user.last_name = json.get('last_name').strip()
+
+        artist.paypalAccount = json.get('paypalAccount')
+
+        if artist.paypalAccount:
+            Assertions.assert_true_raise400('@' in artist.paypalAccount and '.' in artist.paypalAccount,
+                                            translate(language, "ERROR_INVALID_PAYPAL_ACCOUNT"))
+        photo = json.get('photo')
+        Assertions.assert_true_raise400(user.first_name, translate(language, "ERROR_EMPTY_FIRST_NAME"))
+        Assertions.assert_true_raise400(user.last_name, translate(language, "ERROR_EMPTY_LAST_NAME"))
+
+        if artist.phone:
+            Assertions.assert_true_raise400(artist.phone.isnumeric(), translate(language, "ERROR_PHONE_MUST_BE_NUMBER"))
+            Assertions.assert_true_raise400(len(artist.phone) == 9, translate(language, "ERROR_PHONE_LENGTH_9"))
+
+        Assertions.assert_true_raise400(len(user.first_name) > 1,
+                                        translate(language, "ERROR_FIRST_NAME_LENGTH"))
+        Assertions.assert_true_raise400(len(user.last_name) > 1,
+                                        translate(language, "ERROR_LAST_NAME_LENGTH"))
+        if photo:
+            Assertions.assert_true_raise400(photo.startswith(('http://', "https://")),
+                                            translate(language, "ERROR_INVALID_PHOTO_URL_HTTP"))
+            Assertions.assert_true_raise400(Strings.url_is_an_image(photo),
+                                            translate(language, "ERROR_INVALID_PHOTO_URL_ENDFORMAT"))
+
+        artistic_name = json.get('artisticName')
+
+        Assertions.assert_true_raise400(artistic_name, translate(language, "ERROR_NO_ARTISTIC_NAME"))
+        Assertions.assert_true_raise400(artistic_name != "", translate(language, "ERROR_NO_ARTISTIC_NAME"))
+
+        try:
+            portfolio = Portfolio.objects.get(artist=artist)
+
+            portfolio.artisticName = artistic_name
+            user.save()
+
+            artist.user = user
+
+            portfolio.save()
+
+            return artist
+        except Portfolio.DoesNotExist:
+            Assertions.assert_true_raise400(False, translate(language, "ERROR_NOTFOUND_PORTFOLIO"))
+
+    @staticmethod
+    def validate_artist(request):
+        language = check_accept_language(request)
+
+        # Empty validations
+
+        Assertions.assert_true_raise400(request.data, translate(language, "ERROR_EMPTY_FORM"))
+        Assertions.assert_true_raise400(request.data.get("username"), translate(language, "ERROR_EMPTY_USERNAME"))
+        Assertions.assert_true_raise400(request.data.get("password"), translate(language, "ERROR_EMPTY_PASSWORD"))
+        Assertions.assert_true_raise400(request.data.get("confirm_password"),
+                                        translate(language, "ERROR_EMPTY_CONFIRM_PASSWORD"))
+        Assertions.assert_true_raise400(
+            request.data.get("password").strip() == request.data.get("confirm_password").strip(),
+            translate(language, "ERROR_PASSWORD_&_CONFIRM_MUST_BE_EQUALS"))
+        Assertions.assert_true_raise400(request.data.get("email"), translate(language, "ERROR_EMPTY_EMAIL"))
+        Assertions.assert_true_raise400(request.data.get("first_name"), translate(language, "ERROR_EMPTY_FIRST_NAME"))
+        Assertions.assert_true_raise400(request.data.get("last_name"), translate(language, "ERROR_EMPTY_LAST_NAME"))
+        Assertions.assert_true_raise400(check_special_characters_and_numbers(request.data.get("first_name")),
+                                        translate(language, "ERROR_FIRST_NAME_SPECIAL_CHARACTERS"))
+        Assertions.assert_true_raise400(check_special_characters_and_numbers(request.data.get("last_name")),
+                                        translate(language, "ERROR_LAST_NAME_SPECIAL_CHARACTERS"))
+
         username = request.data.get("username").strip()
-        confirm_password = request.data.get("confirm_password").strip()
+        password = request.data.get("password").strip()
         email = request.data.get("email").strip()
         first_name = request.data.get("first_name").strip()
         last_name = request.data.get("last_name").strip()
+
+        user_names = User.objects.values_list('username', flat=True)
+        emails = User.objects.values_list('email', flat=True)
+
         phone = request.data.get("phone")
         photo = request.data.get("photo")
-        Assertions.assert_true_raise400(request.data, {'error': "Empty form is not valid"})
-
-        # Empty validations
-        Assertions.assert_true_raise400(username, {'error': "Username field not provided"})
-        Assertions.assert_true_raise400(password, {'error': "Password field not provided"})
-        Assertions.assert_true_raise400(email, {'error': "Email field not provided"})
-        Assertions.assert_true_raise400(first_name, {'error': "First name not provided"})
-        Assertions.assert_true_raise400(last_name, {'error': "Last name not provided"})
-        Assertions.assert_true_raise400(password == confirm_password, {'error': "Password and confirmation must match"})
 
         # Email in use validation
-        Assertions.assert_true_raise400(not(email in emails), {'error': "Email already in use"})
+
+        Assertions.assert_true_raise400(not (email in emails), translate(language, "ERROR_EMAIL_IN_USE"))
 
         # Password validations
-        Assertions.assert_true_raise400(not(username in password or password in username),
-                                        {'error': "Password can't be similar to the username"})
+
+        Assertions.assert_true_raise400(not (username in password or password in username),
+                                        translate(language, "ERROR_PASSWORD_SIMILAR_USERNAME"))
 
         Assertions.assert_true_raise400(not (email in password or password in email),
-                                        {'error': "Password can't be similar to the email"})
+                                        translate(language, "ERROR_PASSWORD_SIMILAR_EMAIL"))
 
         Assertions.assert_true_raise400(not (first_name in password or password in first_name),
-                                        {'error': "Password can't be similar to the first name"})
+                                        translate(language, "ERROR_PASSWORD_SIMILAR_FIRST_NAME"))
 
         Assertions.assert_true_raise400(not (last_name in password or password in last_name),
-                                        {'error': "Password can't be similar to the last name"})
+                                        translate(language, "ERROR_PASSWORD_SIMILAR_LAST_NAME"))
 
         Assertions.assert_true_raise400('123' not in password and 'qwerty' not in password and
-                                        not password.isnumeric(), {'error':  "Password must be complex"})
+                                        not password.isnumeric(), translate(language, "ERROR_PASSWORD_MUST_BE_COMPLEX"))
 
-        Assertions.assert_true_raise400(len(password) > 7, {'error': "Password is too short"})
+        Assertions.assert_true_raise400(len(password) > 7, translate(language, "ERROR_PASSWORD_IS_TOO_SHORT"))
 
-        Assertions.assert_true_raise400(username not in user_names, {'error': "Username already in use"})
+        Assertions.assert_true_raise400(username not in user_names, translate(language, "ERROR_USERNAME_IN_USE"))
 
         if phone:
-            Assertions.assert_true_raise400(phone.isnumeric(), {'error': "Phone must be a number"})
-            Assertions.assert_true_raise400(len(phone) == 9, {'error': "Phone length must be 9 digits"})
+            Assertions.assert_true_raise400(phone.isnumeric(), translate(language, "ERROR_PHONE_MUST_BE_NUMBER"))
+            Assertions.assert_true_raise400(len(phone) == 9, translate(language, "ERROR_PHONE_LENGTH_9"))
 
-        Assertions.assert_true_raise400(len(first_name) > 1,
-                                        {'error': "First name is too short"})
-        Assertions.assert_true_raise400(len(last_name) > 1,
-                                        {'error': "Last name is too short"})
+        Assertions.assert_true_raise400(len(first_name) > 1, translate(language, "ERROR_FIRST_NAME_LENGTH"))
+        Assertions.assert_true_raise400(len(last_name) > 1, translate(language, "ERROR_LAST_NAME_LENGTH"))
 
-        Assertions.assert_true_raise400(len(email) > 5, {'error': "Email is too short"})
-        Assertions.assert_true_raise400('@' in email and '.' in email, {'error': "Invalid email"})
+        Assertions.assert_true_raise400(len(email) > 5, translate(language, "ERROR_EMAIL_IS_TOO_SHORT"))
+        Assertions.assert_true_raise400('@' in email and '.' in email, translate(language, "ERROR_EMAIL_INVALID"))
 
         if photo:
-
-            Assertions.assert_true_raise400(photo.startswith('http'), {'error': 'Invalid photo url,'
-                                                                       ' the photo must start with http'})
-            Assertions.assert_true_raise400(Strings.url_is_an_image(photo), {'error': 'Invalid photo url,'
-                                                              ' the photo must end with an image extension'})
-        return True
-
+            Assertions.assert_true_raise400(photo.startswith(('http://', "https://")),
+                                            translate(language, "ERROR_INVALID_PHOTO_URL_HTTP"))
+            Assertions.assert_true_raise400(Strings.url_is_an_image(photo),
+                                            translate(language, "ERROR_INVALID_PHOTO_URL_ENDFORMAT"))
+            return True
