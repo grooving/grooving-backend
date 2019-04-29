d5b8a76f7fbe59b9b68c3038c906c4cc193d1d7b
Manuel <bwer23@gmail.com>
2019-04-29 23:08:13 +0200

    Subida traduccion

--- portfolio/views.py
+++ portfolio/views.py
@@ -5,6 +5,8 @@ from rest_framework import generics
 from .serializers import PortfolioSerializer
 from rest_framework import status
 from utils.Assertions import Assertions
+from .internationalization import translate
+from utils.utils import check_accept_language
 
 
 class PortfolioManager(generics.RetrieveUpdateDestroyAPIView):
@@ -15,11 +17,12 @@ class PortfolioManager(generics.RetrieveUpdateDestroyAPIView):
     def get_object(self, pk=None):
         if pk is None:
             pk = self.kwargs['pk']
+        language = check_accept_language(self.request)
         try:
             return Portfolio.objects.get(pk=pk)
         except Portfolio.DoesNotExist:
             Assertions.assert_true_raise404(False,
-                                            {'error': 'Portfolio not found'})
+                                            translate(language, 'ERROR_NO_PORTFOLIO'))
 
     def get(self, request, pk=None, format=None):
         if pk is None:
@@ -31,15 +34,17 @@ class PortfolioManager(generics.RetrieveUpdateDestroyAPIView):
     def put(self, request, pk=None):
         if pk is None:
             pk = self.kwargs['pk']
+        language = check_accept_language(self.request)
         if Portfolio.objects.filter(pk=pk).first():
             portfolio = Portfolio.objects.filter(pk=pk).first()
             loggedUser = get_logged_user(request)
             user_type = get_user_type(loggedUser)
             artist = Artist.objects.filter(portfolio=portfolio).first()
-            if loggedUser is not None and loggedUser.id == artist.id and user_type == "Artist":
+            Assertions.assert_true_raise403(loggedUser is not None, translate(language, 'ERROR_NOT_LOGGED_IN'))
+            if loggedUser.id == artist.id and user_type == "Artist":
                 serializer = PortfolioSerializer(portfolio, data=request.data, partial=True)
                 if serializer.is_valid():
-                    serializer.save(loggedUser)
+                    serializer.save(loggedUser,language)
                     portfolio = self.get_object(pk)
                     serializer = PortfolioSerializer(portfolio, data=serializer.data, partial=True)
                     serializer.is_valid()
@@ -47,9 +52,9 @@ class PortfolioManager(generics.RetrieveUpdateDestroyAPIView):
                 else:
                     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
             else:
-                raise Assertions.assert_true_raise403(False, {'error': 'Not logged or not owner of Portfolio'})
+                raise Assertions.assert_true_raise403(False, translate(language, 'ERROR_NO_PORTFOLIO_USER'))
         else:
-            return Assertions.assert_true_raise404(False, {'error': 'Portfolio not found'})
+            return Assertions.assert_true_raise404(False, translate(language, 'ERROR_NO_PORTFOLIO'))
 
     def delete(self, request, pk=None, format=None):
         if pk is None:
