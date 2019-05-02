e193aae116516ef265bc0569ec3bc490d5870a85
chainwarden16 <whiteraven1000@gmail.com>
2019-05-02 04:01:29 +0200

    Tests funcionales de calendar
    Se ha añadido una serie de test funcionales de edit calendar, tanto positivoss como negativos por valores no permitidos, permisos de usuario y formato incorrecto.

--- calendars/tests.py
+++ calendars/tests.py
@@ -1,5 +1,4 @@
-'''from Grooving.models import  Artist, Portfolio, User,  PaymentPackage, Customer, EventLocation, Zone, \
-    Performance, SystemConfiguration
+from Grooving.models import  Artist, Portfolio, User, Customer, Calendar, Zone, SystemConfiguration
 from django.contrib.auth.hashers import make_password
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITransactionTestCase
@@ -20,6 +19,8 @@ class CalendarTestCase(APITransactionTestCase):
                                              email=your_email)
         Token.objects.create(user=user1_artist10)
 
+        user1_artist10.save()
+
         print('---- Creating artist ----')
 
         artist1 = Artist.objects.create(user=user1_artist10, rating=5.0, phone='600304999',
@@ -27,27 +28,56 @@ class CalendarTestCase(APITransactionTestCase):
                                         photo='https://img.discogs.com/jgyNBtPsY4DiLegwMrOC9N_yOc4=/600x600/smart/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/A-1452461-1423476836-6354.jpeg.jpg',
                                         iban='ES6621000418401234567891', paypalAccount='tamta.info@gmail.com')
 
+        artist1.save()
+
         user2_customer1 = User.objects.create(username='customer1', password=make_password('customer1customer1'),
                                               first_name='Rafael', last_name='Esquivias Ramírez',
                                               email=your_email)
+
+        user2_customer1.save()
+
         Token.objects.create(user=user2_customer1)
+
+        user1_artist2 = User.objects.create(username='artist2', password=make_password('artist2artist2'),
+                                             first_name='Pablo', last_name='Motos',
+                                             email=your_email)
+        Token.objects.create(user=user1_artist2)
+
+        user1_artist2.save()
+
+        artist2 = Artist.objects.create(user=user1_artist2, rating=5.0, phone='600304999',
+                                        language='en',
+                                        photo='https://img.discogs.com/jgyNBtPsY4DiLegwMrOC9N_yOc4=/600x600/smart/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/A-1452461-1423476836-6354.jpeg.jpg',
+                                        iban='ES6621000418401234567891', paypalAccount='tamta.info@gmail.com')
+
+        artist2.save()
 
         print('---- Creating customer ----')
 
-        customer1 = Customer.objects.create(user=user2_customer1, phone='639154189', holder='Rafael Esquivias Ramírez',
-                                            expirationDate='2020-10-01', number='4651001401188232',
-                                            language='en',
-                                            paypalAccount='rafesqram@gmail.com')
+        customer1 = Artist.objects.create(user=user2_customer1, rating=5.0, phone='600304999',
+                                        language='en',
+                                        photo='https://img.discogs.com/jgyNBtPsY4DiLegwMrOC9N_yOc4=/600x600/smart/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/A-1452461-1423476836-6354.jpeg.jpg',
+                                        iban='ES6621000418401234567891', paypalAccount='tamto.info@gmail.com')
+
+        customer1.save()
+
+
 
         print('---- Creating zone ----')
 
         zone1 = Zone.objects.create(name='Andalucía')
+
+        zone1.save()
 
         print('---- Creating portfolio ----')
 
         portfolio1 = Portfolio.objects.create(artist=artist1, artisticName="Los rebujitos")
         portfolio1.zone.add(zone1)
         portfolio1.save()
+
+        calendar = Calendar.objects.create(days=[], portfolio=portfolio1)
+
+        calendar.save()
 
         SystemConfiguration.objects.create(minimumPrice=20.0, currency='EUR', paypalTax='3.4', creditCardTax='1.9',
                                            vat='21',
@@ -63,15 +93,26 @@ class CalendarTestCase(APITransactionTestCase):
                                            aboutUs_es='Sobre nosotros',
                                            termsText_es='Términos y condiciones',
                                            termsText_en='Terms and conditions')
+
     # Driver function
 
-    def test_driver_create_offer(self):
+    def test_driver_edit_calendar(self):
 
-        print('Start test')
-        data1 = {"username": "customer1", "password": "customer1customer1"}
+        print('Start test - edit calendar as the intended artist')
+        data1 = {"username": "artist1", "password": "artist1artist1"}
         response = self.client.post("/api/login/", data1, format='json')
 
         token_num = response.get('x-auth')
+
+        data3 = {"username": "artist2", "password": "artist2artist2"}
+        response3 = self.client.post("/api/login/", data3, format='json')
+
+        token_num3 = response3.get('x-auth')
+
+        data2 = {"username": "customer1", "password": "customer1customer1"}
+        response2 = self.client.post("/api/login/", data2, format='json')
+
+        token_num2 = response2.get('x-auth')
 
         #Para evitar problemas si el token no existe en bd
         token = ''
@@ -79,18 +120,53 @@ class CalendarTestCase(APITransactionTestCase):
             token = Token.objects.all().filter(pk=token_num).first().key
         except:
             pass
+        token2 = ''
+        try:
+            token2 = Token.objects.all().filter(pk=token_num2).first().key
+        except:
+            pass
+        token3 = ''
+        try:
+            token3 = Token.objects.all().filter(pk=token_num3).first().key
+        except:
+            pass
         # Data payload
-        portfolio1 = Portfolio.objects.get(artisticName="Los rebujitos")
-        payload = [
-                # Test positivo, crear un calendar sin incidencias
-                [token, ['2019-09-20'], portfolio1.id, 201]]#,
-                # Test negativo - Fecha incorrecta
-                #[token, 'Descripcion2', '2019 10:00:00', 1, portfolio1.id, 400],
-                # Test negativo - Descripcion incorrecta
-                #[token, '', '2019-05-10T10:00:00', 1, portfolio1.id, 400]]
+        portfolio1 = Portfolio.objects.all().first().id
 
+        print(token)
+
+        payload = [
+
+            #Test positivo 1, con dia
+            [token, ['2019-09-20'], portfolio1, 200],
+            #Test positivo 2, sin dias
+            [token, [], portfolio1, 200],
+            #Test negativo 1, fecha equivocada
+            [token, ['2019/08/20'], portfolio1, 400],
+            # Test negativo 2, id invalido
+            [token, ['2019-08-20'], 'a', 400],
+            # Test negativo 3, no se pasan días al calendar
+            [token, '', 'a', 400],
+            # Test negativo 4, no se pasan días al calendar (ahora es un None)
+            [token, None, portfolio1, 400],
+            # Test negativo 5, no se pasa un id de portfolio
+            [token, [], None, 400],
+            # Test negativo 6, se pasa un id 0 de portfolio
+            [token, [], 0, 400],
+            # Test negativo 7, customer
+            [token2, ['2019-09-20'], portfolio1, 403],
+            # Test negativo 8, artist incorrecto
+            [token3, [], portfolio1, 403],
+            #Test negativo 9, no se pasa un array
+            [token, '2019-09-14', portfolio1, 400]
+
+        ]
+        contador = 0
         for data in payload:
-            self.template_create_offer(data)
+            contador = contador + 1
+            print('Test número '+str(contador))
+            print(str(data))
+            self.template_edit_calendar(data)
 
     def generateData(self, args):
         return {'days': args[1],
@@ -98,16 +174,16 @@ class CalendarTestCase(APITransactionTestCase):
 
     # Template function
 
-    def template_create_offer(self, args):
+    def template_edit_calendar(self, args):
 
         data = self.generateData(args)
 
-        response_es = self.client.post('/offer/', data, format='json', HTTP_AUTHORIZATION='Token ' + args[0],
+        response_es = self.client.put('/calendar/1/', data, format='json', HTTP_AUTHORIZATION='Token ' + args[0],
                                     HTTP_ACCEPT_LANGUAGE='es')
 
         self.assertEqual(args[-1], response_es.status_code)
 
-        response_en = self.client.post('/offer/', data, format='json', HTTP_AUTHORIZATION='Token ' + args[0],
-                                    HTTP_ACCEPT_LANGUAGE='en')
+        response_en = self.client.put('/calendar/1/', data, format='json', HTTP_AUTHORIZATION='Token ' + args[0],
+                                   HTTP_ACCEPT_LANGUAGE='en')
 
-        self.assertEqual(args[-1], response_en.status_code)'''
+        self.assertEqual(args[-1], response_en.status_code)
