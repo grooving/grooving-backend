3eea6f6809520bdbfc3c326e7e0a2b6ce0f832aa
UtrillaJC <utri1990@gmail.com>
2019-05-03 22:39:26 +0200

    admin improvements

--- Grooving/admin.py
+++ Grooving/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import SystemConfiguration
+from django.contrib.auth.models import User, Group
+from django.contrib import admin
 
 admin.site.register(SystemConfiguration)
+admin.site.unregister(User)
+admin.site.unregister(Group)
