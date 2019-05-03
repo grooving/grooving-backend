aa875ff93dada93fcccf3c89a9429fc210b37e0b
chainwarden16 <whiteraven1000@gmail.com>
2019-05-03 10:29:15 +0200

    Error para cadenas de caracteres muy largas
    Se encuentra en la internacionalización de utils.

--- utils/internationalization.py
+++ utils/internationalization.py
@@ -2,14 +2,16 @@ spanish = {
 
     "ERROR_VALUE_NULL": "La ID no puede ser nula.",
     "ERROR_OBJECT_NOT_FOUND": "No hay un objeto que corresponda con esta ID.",
-    "ERROR_NOT_A_VALID_ID": "La ID introducida no es válida."
+    "ERROR_NOT_A_VALID_ID": "La ID introducida no es válida.",
+    "ERROR_STRING_TOO_LONG": "La cadena de caracteres es demasiado larga."
 }
 
 english = {
 
     "ERROR_VALUE_NULL": "The ID cannot be null.",
     "ERROR_OBJECT_NOT_FOUND": "The object you are trying to retrieve does not exist.",
-    "ERROR_NOT_A_VALID_ID": "The given ID is not valid."
+    "ERROR_NOT_A_VALID_ID": "The given ID is not valid.",
+    "ERROR_STRING_TOO_LONG": "The string is too long"
 
 }
 
